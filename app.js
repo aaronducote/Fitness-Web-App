080faf43415c9bf7c54d62dedfb66f46ae7390e4
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2023-05-25 15:59:33 -0700

    step 1 complete for app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express');
 var app = express();
 PORT = 5741;
 
+var db = require('./database/db-connector');
 
 /* 
  * ROUTES
@@ -15,7 +16,21 @@ PORT = 5741;
 
 app.get('/', function(req, res)
 	{
-		res.send("The server is running");
+		query1 = 'DROP TABLE IF EXISTS diagnostic;';
+        	query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+        	query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+	        query4 = 'SELECT * FROM diagnostic;';
+
+		db.pool.query(query1, function(err, results, fields) {
+			db.pool.query(query2, function(err, results, fields) {
+				db.pool.query(query3, function(err, results, fields) {
+					db.pool.query(query4, function(err, results, fields) {
+						let base = "<h1>MySQL Results:</h1>";
+						res.send(base + JSON.stringify(results));
+					});
+				});
+			});
+		});
 	});
 
 /*
