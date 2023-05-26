0b806f503f8b0d2ac4979231d2cd9e170158e3fe
aaronducote <ducotea@oregonstate.edu>
2023-05-25 17:01:02 -0700

    updated update_member

--- public/update_member.js
+++ public/update_member.js
@@ -1,5 +1,5 @@
 // Get the objects we need to modify
-let updatePersonForm = document.getElementById('update-person-form-ajax');
+let updateMemberForm = document.getElementById('update-member-form-ajax');
 
 // Modify the objects we need
 updatePersonForm.addEventListener("submit", function (e) {
@@ -8,31 +8,27 @@ updatePersonForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
     // Get form fields we need to get data from
-    let inputFullName = document.getElementById("mySelect");
-    let inputHomeworld = document.getElementById("input-homeworld-update");
+    let inputName = document.getElementById("mySelect");
+    let inputEmail = document.getElementById("input-Email-update");
 
     // Get the values from the form fields
-    let fullNameValue = inputFullName.value;
-    let homeworldValue = inputHomeworld.value;
+    let NameValue = Name.value;
+    let EmailValue = Email.value;
     
     // currently the database table for bsg_people does not allow updating values to NULL
     // so we must abort if being bassed NULL for homeworld
 
-    if (isNaN(homeworldValue)) 
-    {
-        return;
-    }
 
 
     // Put our data we want to send in a javascript object
     let data = {
-        fullname: fullNameValue,
-        homeworld: homeworldValue,
+        Name: NameValue,
+        Email: EmailValue,
     }
     
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", "/put-person-ajax", true);
+    xhttp.open("PUT", "/put-member-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
@@ -54,17 +50,17 @@ updatePersonForm.addEventListener("submit", function (e) {
 })
 
 
-function updateRow(data, personID){
+function updateRow(data, memberID){
     let parsedData = JSON.parse(data);
     
-    let table = document.getElementById("people-table");
+    let table = document.getElementById("member-table");
 
     for (let i = 0, row; row = table.rows[i]; i++) {
        //iterate through rows
        //rows would be accessed using the "row" variable assigned in the for loop
-       if (table.rows[i].getAttribute("data-value") == personID) {
+       if (table.rows[i].getAttribute("data-value") == memberID) {
 
-            // Get the location of the row where we found the matching person ID
+            // Get the location of the row where we found the matching member ID
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
             // Get td of homeworld value
